f494ae9c8d6764a9549976f1fffe9eb055fda81d
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-10-08 17:28:47 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# @zero-dependency/tmp
+
+[![](https://img.shields.io/npm/v/@zero-dependency/tmp)](https://npm.im/@zero-dependency/tmp)
+![](https://img.shields.io/npm/l/@zero-dependency/tmp)
+
+## Installation
+
+```sh
+npm install @zero-dependency/tmp
+```
+
+```sh
+yarn add @zero-dependency/tmp
+```
+
+```sh
+pnpm add @zero-dependency/tmp
+```
+
+## Usage
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+/// <reference types="vitest" />
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import { name } from './package.json'
+
+export default defineConfig({
+  plugins: [dts()],
+  build: {
+    sourcemap: true,
+    target: 'esnext',
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      fileName: (format) => `index.${format}.js`,
+      name
+    }
+  },
+  test: {
+    watch: false,
+    threads: true,
+    logHeapUsage: true,
+    environment: 'jsdom'
+  }
+})
